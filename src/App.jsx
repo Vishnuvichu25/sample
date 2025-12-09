ed28d722b8f6052036efcd24632f2e7701fdbdef
Vishnuvichu25 <vishnu25narayanan@gmail.com>
2025-12-09 12:01:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Home'
+import Mking from './Firsy'
+const App = () => {
+  return (
+    <div>
+      <Mking/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Firsy.jsx
@@ -0,0 +1,8 @@
+function Mking(){
+    return(
+        <div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor nihil labore quas, ab iusto libero molestiae itaque a! Rerum ab exercitationem vero tempore animi provident dolorem laboriosam nihil ducimus ea.</p>
+        </div>
+    )
+}
+export default Mking
